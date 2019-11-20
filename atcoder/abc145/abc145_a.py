6385d5572bded9c2a1a1b35fc7760211283219d5
necoak <kaneko1983g@gmail.com>
2019-11-21 07:42:56 +0900

    #Atcoder : abc145_a

---
+++ atcoder/abc145/abc145_a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Atcoder necoak answer
+# https://atcoder.jp/contests/abc126/tasks/abc145_a
+
+circle_r_text = input()
+circle_r = int(circle_r_text)
+
+circle_area = circle_r ** 2;
+print(circle_area)
