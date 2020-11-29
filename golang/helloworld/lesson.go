82d9629e66a92a5eadee49a6ed00c839449e2377
necoak <kaneko1983g@gmail.com>
2020-11-30 00:02:39 +0900

    golang: type conversion

--- golang/helloworld/lesson.go
+++ golang/helloworld/lesson.go
@@ -2,25 +2,23 @@ package main // mainパッケージの用意が必要
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	var t, f bool = true, false
-	//t, f := true, false // これでもいい
-	fmt.Printf("%T %v\n", t, t)
-	fmt.Printf("%T %v\n", f, f)
 
-	fmt.Printf("%T %v %t\n", 0, 0, 0)
-	fmt.Printf("%T %v %t\n", 1, 1, 1)
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
 
-	fmt.Println(true && true)
-	fmt.Println(true && false)
-	fmt.Println(false && false)
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
 
-	fmt.Println(true || true)
-	fmt.Println(true || false)
-	fmt.Println(false || false)
+	i, _ := strconv.Atoi("14")
+	fmt.Printf("%T %v\n", i, i)
 
-	fmt.Println(!true)
-	fmt.Println(!false)
+	h := "Hello World"
+	fmt.Printf("%T %v\n", h[0], h[0])
+	fmt.Printf("%T %v\n", string(h[0]), string(h[0]))
 }
