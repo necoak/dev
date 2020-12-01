6d3fd439630e796640013c6a30cbc76437e82f00
necoak <kaneko1983g@gmail.com>
2020-12-01 23:52:50 +0900

    #golang: slice

--- golang/helloworld/lesson.go
+++ golang/helloworld/lesson.go
@@ -6,20 +6,31 @@ import (
 
 func main() {
 
-	// array
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
 
-	/*
-		// array
-		var b [2]int = [2]int{100, 200}
-		fmt.Println(b)
-	*/
-	// slice
-	// slice can be append.
-	var b []int = []int{100, 200}
-	b = append(b, 300)
-	fmt.Println(b)
+	fmt.Println("---------------")
+
+	n[2] = 100
+	fmt.Println(n)
+
+	fmt.Println("---------------")
+
+	var board = [][]int{
+		[]int{0, 1, 2},
+		[]int{3, 4, 5},
+		[]int{6, 7, 8},
+	}
+	fmt.Println(board)
+
+	fmt.Println("---------------")
+
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
+
 }
