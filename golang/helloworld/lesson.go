44b05ccdbafee5f5c03bd8b5628e97484749c1c3
necoak <kaneko1983g@gmail.com>
2020-12-04 23:53:05 +0900

    golang: function

--- golang/helloworld/lesson.go
+++ golang/helloworld/lesson.go
@@ -2,33 +2,20 @@ package main // mainパッケージの用意が必要
 
 import "fmt"
 
-func my_print(var_name string, val []int) {
-	fmt.Printf("["+var_name+"] len=%d cap=%d value=%v\n", len(val), cap(val), val)
+func calc(x int, y int) (int, int) {
+	return x + y, x - y
+}
+
+func calc2(price int, item int) (result int) {
+	result = price * item
+	// return result
+	return
 }
 
 func main() {
+	r1, r2 := calc(10, 20)
+	fmt.Println(r1, r2)
 
-	m := map[string]int{"apple": 100, "banana": 200}
-	fmt.Println(m)
-	fmt.Println(m["apple"])
-	m["banana"] = 300
-	fmt.Println(m)
-	m["new"] = 500
-	fmt.Println(m)
-
-	fmt.Println(m["nothing"])
-
-	v, ok := m["apple"]
-	fmt.Println(v, ok)
-
-	v2, ok2 := m["nothing"]
-	fmt.Println(v2, ok2)
-
-	m2 := make(map[string]int)
-	m2["pc"] = 5000
-	fmt.Println(m2)
-
-	var m3 map[string]int
-	m3["pc"] = 5000
-	fmt.Println(m3)
+	r3 := calc2(100, 2)
+	fmt.Println(r3)
 }
