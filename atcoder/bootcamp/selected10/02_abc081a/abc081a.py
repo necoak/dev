9f92be22c59e35fd75e7fe135db6831535898270
necoak <kaneko1983g@gmail.com>
2021-03-01 20:05:31 +0900

    atcoder: abc081-b (selected10-02) is done.

---
+++ atcoder/bootcamp/selected10/02_abc081a/abc081a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Atcoder necoak answer
+# https://atcoder.jp/contests/abc081/tasks/abc081_a
+from collections import Counter
+
+s = input()
+counter = Counter(s)
+print(counter['1'])
