34fcde6b28fe4d8d1d6321a85f1319c592cdfb80
necoak <kaneko1983g@gmail.com>
2021-03-11 23:57:34 +0900

    atcoder: abcc136d is doing (TLE is remain).

--- atcoder/bootcamp/hard100/001/abc136d.py
+++ atcoder/bootcamp/hard100/001/abc136d.py
@@ -2,17 +2,14 @@
 # Atcoder necoak answer
 # https://atcoder.jp/contests/abc136/tasks/abc136_d
 
-import sys
-
 s = list(input())
 
 child_list = [1 for v in range(len(s))]
 child_patterns = []
+child_patterns.append(child_list)
 ans = None
 
-for x in range(10**100):
-    
-    child_patterns.append([child_list])
+for x in range(1, 10**100+1, 1):
 
     next_child_list = [0 for v in range(len(s))]
     for i,si in enumerate(s):
@@ -24,14 +21,14 @@ for x in range(10**100):
     try:
         izen_no_index = child_patterns.index(next_child_list)
     except ValueError:
+        child_patterns.append(next_child_list)
         child_list = next_child_list
         continue
 
-    print(child_list)
     if x == izen_no_index:
         ans = child_list
     else:
-        ans_pattern_index = ((10**100 - 1 - izen_no_index) % (x - izen_no_index)) +  izen_no_index
+        ans_pattern_index = ((10**100 - izen_no_index) % (x - izen_no_index)) + izen_no_index
         ans = child_patterns[ans_pattern_index]
     break
 
