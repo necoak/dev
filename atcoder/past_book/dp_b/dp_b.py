d4890fec90a416da7aae50663f39c12e5ef222c3
necoak <kaneko1983g@gmail.com>
2021-03-29 23:55:33 +0900

    atcoder: dp_b is doing(including WA, TLE).

--- atcoder/past_book/dp_b/dp_b.py
+++ atcoder/past_book/dp_b/dp_b.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Atcoder necoak answer
-# https://atcoder.jp/contests/dp/tasks/dp_a
+# https://atcoder.jp/contests/dp/tasks/dp_b
 
 N, K = list(map(int, input().split()))
 h = list(map(int, input().split()))
@@ -12,8 +12,8 @@ min_cost[1] = abs(h[0]-h[1])
 
 for i in range(2, N, 1):
     
-    for j in range(K):
-        min_cost[i] = min(min_cost[i], min_cost[i-(j+1)]+abs(h[i]-h[i-(j+1)]))
+    for j in range(i-1, i-K-1, -1):
+        if 0 <= j < N:
+            min_cost[i] = min(min_cost[i], min_cost[j]+abs(h[i]-h[j]))
 
 print(min_cost[N-1])
-
