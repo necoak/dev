6e0722d497929db351a7f420f057b164e20bd239
necoak <kaneko1983g@gmail.com>
2021-03-30 23:48:33 +0900

    atcoder: dp_b cant solved.

--- atcoder/past_book/dp_b/dp_b.py
+++ atcoder/past_book/dp_b/dp_b.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Atcoder necoak answer
 # https://atcoder.jp/contests/dp/tasks/dp_b
+import sys 
+from collections import Counter
 
 N, K = list(map(int, input().split()))
 h = list(map(int, input().split()))
@@ -8,12 +10,10 @@ h = list(map(int, input().split()))
 min_cost = [10**4] * N
 
 min_cost[0] = 0
-min_cost[1] = abs(h[0]-h[1])
+#min_cost[1] = abs(h[0]-h[1])
 
-for i in range(2, N, 1):
-    
-    for j in range(i-1, i-K-1, -1):
-        if 0 <= j < N:
-            min_cost[i] = min(min_cost[i], min_cost[j]+abs(h[i]-h[j]))
+for next_i in range(1, N, 1):
+    for now_i in range( max(, 0), min(i-1, N), 1):
+        min_cost[i] = min(min_cost[i], min_cost[before_i]+abs(h[i]-h[before_i]))
 
 print(min_cost[N-1])
