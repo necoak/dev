9a99b377ebcee8345baa313a963ddd99ec58b516
necoak <kaneko1983g@gmail.com>
2021-03-31 23:34:58 +0900

    atcoder:  dp_b is  done.

--- atcoder/past_book/dp_b/dp_b.py
+++ atcoder/past_book/dp_b/dp_b.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 # Atcoder necoak answer
 # https://atcoder.jp/contests/dp/tasks/dp_b
-import sys 
-from collections import Counter
 
 N, K = list(map(int, input().split()))
 h = list(map(int, input().split()))
 
-min_cost = [10**4] * N
+min_cost = [float('inf')] * N
 
 min_cost[0] = 0
-#min_cost[1] = abs(h[0]-h[1])
 
-for next_i in range(1, N, 1):
-    for now_i in range( max(, 0), min(i-1, N), 1):
-        min_cost[i] = min(min_cost[i], min_cost[before_i]+abs(h[i]-h[before_i]))
+for now_i in range(N):
+    for next_i in range( max(now_i+1, 0), min(now_i+K+1, N), 1):
+        min_cost[next_i] =  min(min_cost[next_i], min_cost[now_i] + abs(h[now_i]-h[next_i]))
 
 print(min_cost[N-1])
