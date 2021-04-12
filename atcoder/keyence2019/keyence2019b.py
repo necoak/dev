6b95526a96f665f9faebb2e3515c313dfd2afda9
necoak <kaneko1983g@gmail.com>
2021-04-12 23:57:58 +0900

    atcoder: keyence2019b is doing.

---
+++ atcoder/keyence2019/keyence2019b.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Atcoder necoak answer
+# https://atcoder.jp/contests/keyence2019/tasks/keyence2019_b
+
+
+import sys
+
+S =  input()
+
+KEYENCE_STRING = 'keyence'
+
+keyence_str_index = 0
+
+for s in S:
+    
