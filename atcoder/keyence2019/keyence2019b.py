9194c90b4153ac7d22ed09fc84ad1eaf317ccb4e
necoak <kaneko1983g@gmail.com>
2021-04-13 00:08:42 +0900

    atcoder: keyence2019b is done.

--- atcoder/keyence2019/keyence2019b.py
+++ atcoder/keyence2019/keyence2019b.py
@@ -10,6 +10,15 @@ S =  input()
 KEYENCE_STRING = 'keyence'
 
 keyence_str_index = 0
-
-for s in S:
-    
+remove_len = len(S) - len(KEYENCE_STRING)
+for remove_start_index in range(0, len(S)-remove_len, 1):
+    removed_s = None
+    if remove_start_index == 0:
+        removed_s = S[remove_len:]
+    else:
+        removed_s = S[0:remove_start_index] + S[remove_start_index+remove_len:]
+    #print(remove_start_index, removed_s)
+    if removed_s == KEYENCE_STRING:
+        print('YES')
+        sys.exit(0)
+print('NO')
