18a9831ec59ff0a75dcdc2907d95bbf0e37a2692
necoak <kaneko1983g@gmail.com>
2021-04-22 23:51:43 +0900

    Update abc085c.py
    Atcoder: abc085-c is done.

--- atcoder/abc085/abc085c.py
+++ atcoder/abc085/abc085c.py
@@ -2,16 +2,9 @@
 # Atcoder necoak answer
 # https://atcoder.jp/contests/abs/tasks/abc085_b
 
-import sys
+N = int(input())
+d_list = []
+for i in range(N):
+    d_list.append(int(input()))
 
-N, Y = list(map(int, input().split()))
-
-BILLS = [1000, 5000, 10000]
-
-for i in range(N+1):
-    for j in range(0, N+1-i):
-        k = N - i - j
-        if (10000*i + 5000*j + 1000*k) == Y:
-            print(i, j, k)
-            sys.exit(0)
-print(-1, -1, -1)
+print(len(set(d_list)))
